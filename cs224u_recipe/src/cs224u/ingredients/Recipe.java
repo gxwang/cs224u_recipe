f2134af19caaf4bef2162e6d34ee61d0ae16be8e
benjaminholtz <benjaminholtz@192.168.1.3>
2012-02-28 15:25:19 -0800

    Added a recipe class. Not sure if we will want one, but i brought up some of the issues that we might want to consider if we do.

---
+++ cs224u_recipe/src/cs224u/ingredients/Recipe.java
@@ -0,0 +1,16 @@
+package cs224u.ingredients;
+
+public class Recipe {
+
+	/*
+	 * So I am thinking that we need the following things from the recipe class:
+	 * - plaintext
+	 * - labeled data if possible
+	 * - containers for all of the labels
+	 * 
+	 * But I am not sure if we want to read recipes streaming, or if we should load everything into memory?
+	 * I think this will depend on how big this class gets/whether or not we using this class.
+	 */
+	
+	
+}
