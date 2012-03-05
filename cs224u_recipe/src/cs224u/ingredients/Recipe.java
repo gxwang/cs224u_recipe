5f73b4011cf269d602f1d2183b8fe48db3b5a28f
Ben Holtz <bholtz@cs.stanford.edu>
2012-03-05 01:36:16 -0800

    The recipe class is just coing to serve as a container for recipe data

--- cs224u_recipe/src/cs224u/ingredients/Recipe.java
+++ cs224u_recipe/src/cs224u/ingredients/Recipe.java
@@ -1,16 +1,57 @@
 package cs224u.ingredients;
 
-public class Recipe {
+import java.util.*;
 
-	/*
-	 * So I am thinking that we need the following things from the recipe class:
-	 * - plaintext
-	 * - labeled data if possible
-	 * - containers for all of the labels
-	 * 
-	 * But I am not sure if we want to read recipes streaming, or if we should load everything into memory?
-	 * I think this will depend on how big this class gets/whether or not we using this class.
-	 */
+/**
+ * The Recipe class is just meant to be a container for a single recipe.
+ * @author benjaminholtz
+ *
+ */
+public class Recipe {
+	
+	private ArrayList<String> ingredients;
+	private ArrayList<String> directions;
+	private String plaintext;
+
+	public Recipe() {
+	}
+
+	public Recipe(ArrayList<String> ingredients, ArrayList<String> directions,
+			String plaintext) {
+		this.ingredients = ingredients;
+		this.directions = directions;
+		this.plaintext = plaintext;
+	}
+
+	public ArrayList<String> getIngredients() {
+		return ingredients;
+	}
+	
+	public void setIngredients(ArrayList<String> ingredients) {
+		this.ingredients = ingredients;
+	}
+	
+	public ArrayList<String> getDirections() {
+		return directions;
+	}
+	
+	public void setDirections(ArrayList<String> directions) {
+		this.directions = directions;
+	}
+	
+	public String getPlaintext() {
+		return plaintext;
+	}
+	
+	public void setPlaintext(String plaintext) {
+		this.plaintext = plaintext;
+	}
+
+	@Override
+	public String toString() {
+		return plaintext;
+	}
+	
 	
 	
 }
